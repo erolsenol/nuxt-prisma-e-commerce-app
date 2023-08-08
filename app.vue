40d66b5f18a73d071b2c3b3b705d149f574719b2
erolsenol <erol.senol@outlook.com>
2023-08-08 20:04:02 +0300

    vee-validate add

--- app.vue
+++ app.vue
@@ -3,6 +3,28 @@ const { locale } = useI18n()
 const { isMobile } = useDevice();
 const { $anime } = useNuxtApp()
 const snackbar = useSnackbar();
+import { useForm,Field, Form } from 'vee-validate';
+
+// Validation, or use `yup` or `zod`
+function required(value) {
+  return value ? true : 'This field is required';
+}
+
+// Create the form
+const { defineInputBinds, handleSubmit, errors } = useForm({
+  validationSchema: {
+    field: required,
+  },
+});
+
+// Define fields
+const field = defineInputBinds('field');
+
+// Submit handler
+const onSubmit = handleSubmit(values => {
+  // Submit to API
+  console.log(values);
+});
 
 onMounted(() => {
     $anime({ targets: '.title', translateX: 250, duration: 800 })
@@ -18,10 +40,18 @@ var other = _difference([7,8,9], 2, [3], [[4]]);
 
 <template>
   <div>
-    <SeoKit />
+    <Form v-slot="{ errors }" @submit="onSubmit">
+    <Field name="field" :rules="required" />
+
+    <span>{{ errors.field }}</span>
+
+    <button>Submit</button>
+  </Form>
+ 
     <h1 class="title">Nuxt Anime</h1>
     <div>nasilsin iyi misin ben iyiym</div>
     <NuxtWelcome />
     <NuxtSnackbar />
+    <SeoKit />
   </div>
 </template>
