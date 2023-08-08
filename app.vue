82cdd8237297ba4da68e09d6c961c35bcd130c06
erolsenol <erol.senol@outlook.com>
2023-08-08 18:59:30 +0300

    lodash add

--- app.vue
+++ app.vue
@@ -1,12 +1,14 @@
 <script setup>
 const { locale } = useI18n()
 const { isMobile } = useDevice();
+
+//lodash
+var other = _difference([7,8,9], 2, [3], [[4]]);
 </script>
 
 
 <template>
   <div>
-    
     <NuxtWelcome />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,15 +3,16 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
-  //   ['@nuxtjs/eslint-module', { ...eslint }]
-  '@nuxtjs/i18n',
-  '@nuxt/image',
-  '@nuxtjs/device',
-  'nuxt-icon',
-  'nuxt-swiper'
+    //   ['@nuxtjs/eslint-module', { ...eslint }]
+    "@nuxtjs/i18n",
+    "@nuxt/image",
+    "@nuxtjs/device",
+    "nuxt-icon",
+    "nuxt-swiper",
+    "nuxt-lodash",
   ],
   i18n: {
-    vueI18n: './i18n.config.ts' // if you are using custom path, default 
+    vueI18n: "./i18n.config.ts", // if you are using custom path, default
   },
   image: {
     // Options
@@ -22,5 +23,16 @@ export default defineNuxtConfig({
     // prefix: 'Swiper',
     // styleLang: 'css',
     // modules: ['navigation', 'pagination'], // all modules are imported by default
-  }
-})
+  },
+  lodash: {
+    prefix: "_",
+    prefixSkip: ["string"],
+    upperAfterPrefix: false,
+    exclude: ["map"],
+    alias: [
+      ["camelCase", "stringToCamelCase"], // => stringToCamelCase
+      ["kebabCase", "stringToKebab"], // => stringToKebab
+      ["isDate", "isLodashDate"], // => _isLodashDate
+    ],
+  },
+});
