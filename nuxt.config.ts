963fbdd72559e873f4d167f378e7ee077cc9cf6a
erolsenol <erol.senol@outlook.com>
2023-08-08 18:54:12 +0300

    icon plugin add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   '@nuxtjs/i18n',
   '@nuxt/image',
   '@nuxtjs/device',
+  'nuxt-icon'
   ],
   i18n: {
     vueI18n: './i18n.config.ts' // if you are using custom path, default 
