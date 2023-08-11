1b38afeca6444bdbd9d10cdd4c105f0102cb9ed6
erolsenol <erol.senol@outlook.com>
2023-08-11 09:56:58 +0300

    quick content add

--- components/QuickAction.vue
+++ components/QuickAction.vue
@@ -7,7 +7,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="card rounded border  border-2 border-secondary border-opacity-75 overflow-hidden" style="width: 10rem; max-height: 15rem;">
+    <div class="card rounded border border-2 border-secondary border-opacity-75 overflow-hidden my-3" style="max-width: 15rem; min-width: 10rem; max-height: 15rem;">
         <div class="card-body">
             <Icon name="emojione-monotone:1st-place-medal" color="black" class="mb-3" size="50" />
             <h5 class="card-title text-capitalize" v-if="title">{{ title }}</h5>
--- pages/index.vue
+++ pages/index.vue
@@ -14,20 +14,7 @@ const { $helper } = useNuxtApp();
 
   <div class="content mt-3">
     <div class="container">
-      <div class="row">
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-          <QuickAction title="test" icon="icontest"
-            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" />
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-          <QuickAction title="test" icon="icontest"
-            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" />
-        </div>
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-          <QuickAction title="test" icon="icontest"
-            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s" />
-        </div>
-      </div>
+     <QuickContent />
     </div>
   </div>
 </template>
