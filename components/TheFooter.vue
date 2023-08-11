128fd4753282b61f166bf7578140cd2dde1ddb98
erolsenol <erol.senol@outlook.com>
2023-08-11 08:21:09 +0300

    footer start

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,11 +1,36 @@
 <script setup>
+
 </script>
 
 <template>
-   <div>footer</div>
-   <!-- <el-footer>Footer</el-footer> -->
+   <div class="footer mt-3">
+      <div class="container pt-3">
+         <div class="row">
+            <div class="col-12 col-sm-6 col-md-4 mb-3">
+               <ul class="list-group list-group-vertical">
+                  <li class="list-group-item"> <h5>An item</h5></li>
+                  <li class="list-group-item">A second item</li>
+                  <li class="list-group-item">A third item</li>
+               </ul>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 mb-3">
+               <ul class="list-group list-group-vertical">
+                  <li class="list-group-item">An item</li>
+                  <li class="list-group-item">A second item</li>
+                  <li class="list-group-item">A third item</li>
+               </ul>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 mb-3">
+               <ul class="list-group list-group-vertical">
+                  <li class="list-group-item">An item</li>
+                  <li class="list-group-item">A second item</li>
+                  <li class="list-group-item">A third item</li>
+               </ul>
+            </div>
+         </div>
+      </div>
+   </div>
 </template>
-
 
 <style>
 
