8b58074500130a66ab26264c6828170cc8c3a875
erolsenol <erol.senol@outlook.com>
2023-08-14 23:33:24 +0300

    wrong

--- components/product/add.vue
+++ components/product/add.vue
@@ -6,6 +6,7 @@ export default {
 
 <script setup>
 import { ref } from "vue";
+const snackbar = useSnackbar();
 // const { $hello, $readFileSync } =  useNuxtApp()
 
 const input_image = ref(null);
@@ -17,6 +18,10 @@ const product = ref({
   images: [],
   imageNames: [],
 });
+const images = ref([])
+const qwe = ref(null)
+
+const formData = ref(new FormData());
 
 function onFileChange(e) {
   let files = e.target.files || e.dataTransfer.files;
@@ -27,7 +32,6 @@ function createImage(files) {
   product.value.imageNames = [];
   product.value.images = [];
 
-
   for (var index = 0; index < files.length; index++) {
     console.log("files[index]", files[index]);
     var reader = new FileReader();
@@ -35,40 +39,59 @@ function createImage(files) {
     reader.onload = (event) => {
       console.log("12312", event);
       const imageUrl = event.target.result;
+
+      product.value.images.push(event.target.result);
+      images.value.push(event.target.result);
+      qwe =  event.target.result;
       product.value.images.push(imageUrl);
     };
+    reader.readAsArrayBuffer(files[index]);
     // reader.readAsText(files[index]);
     // reader.readAsDataURL(files[index]);
 
-    const blob = new Blob([files[index]], { type: files[index].type });
-    reader.readAsArrayBuffer(blob);
+    // const blob = new Blob([files[index]], { type: files[index].type });
+    // reader.readAsArrayBuffer(blob);
+
+    formData.value.append(`file${index}`, files[index]);
+
     // data.value.files.push(blob)
   }
+  console.log("formData", formData);
+  console.log("formData.value", formData.value);
 }
 function removeImage(index) {
   images.splice(index, 1);
 }
 
 async function save() {
-
   const { data, pending, error, refresh } = await useFetch("/api/product", {
     method: "post",
     body: product,
   });
   console.log("save res:", { data, pending, error, refresh });
   console.log("data", data.value);
-  console.log("111");
   if (data.value.status) {
     console.log("trueee");
-    for (let index = 0; index < product.value.images.length; index++) {
-      const imageArrayBuffer = product.value.images[index];
-      const response = await useFetch("/api/image", {
+    console.log("images",images);
+    console.log("images",images.value);
+    const response = await useFetch("/api/image", {
       method: "post",
-      body: imageArrayBuffer,
+      body: images.value,
+      // body: formData,
     });
-    console.log(`request ${index}:`, response.value.data)
+    if (response.value.data.status) {
+      console.log("Görsel Yüklendi");
+      snackbar.add({
+        type: "success",
+        text: "Görsel Yüklendi",
+      });
     }
-   
+  } else {
+    console.log("Ürün Kaydedilemedi");
+    snackbar.add({
+      type: "danger",
+      text: "Ürün Kaydedilemedi",
+    });
   }
 }
 </script>
@@ -79,61 +102,36 @@ async function save() {
     {{ images }}
     <div class="mb-3">
       <label for="product-name" class="form-label">Ürün Adı</label>
-      <input
-        :value="product.name"
-        @change="product.name = $event.target.value"
-        type="text"
-        class="form-control"
-        id="product-name"
-      />
+      <input :value="product.name" @change="product.name = $event.target.value" type="text" class="form-control"
+        id="product-name" />
     </div>
     <div class="mb-3">
       <label for="product-name" class="form-label">Ürün Başlığı</label>
-      <input
-        :value="product.title"
-        @change="product.title = $event.target.value"
-        type="text"
-        class="form-control"
-        id="product-name"
-      />
+      <input :value="product.title" @change="product.title = $event.target.value" type="text" class="form-control"
+        id="product-name" />
     </div>
     <div class="mb-3">
       <label for="product-content" class="form-label">Ürün Açıklaması</label>
-      <input
-        :value="product.content"
-        @change="product.content = $event.target.value"
-        type="text"
-        class="form-control"
-        id="product-content"
-      />
+      <input :value="product.content" @change="product.content = $event.target.value" type="text" class="form-control"
+        id="product-content" />
     </div>
     <div class="product-add-slide mb-3">
-      <Swiper
-        :modules="[SwiperAutoplay, SwiperEffectCreative]"
-        :slides-per-view="1"
-        :loop="true"
-        :effect="'creative'"
+      <Swiper :modules="[SwiperAutoplay, SwiperEffectCreative]" :slides-per-view="1" :loop="true" :effect="'creative'"
         :autoplay="{
           delay: 8000,
           disableOnInteraction: true,
-        }"
-        :creative-effect="{
-          prev: {
-            shadow: false,
-            translate: ['-20%', 0, -1],
-          },
-          next: {
-            translate: ['100%', 0, 0],
-          },
-        }"
-      >
+        }" :creative-effect="{
+  prev: {
+    shadow: false,
+    translate: ['-20%', 0, -1],
+  },
+  next: {
+    translate: ['100%', 0, 0],
+  },
+}">
         <SwiperSlide v-for="(image, index) in product.images" :key="index">
           <div class="card" style="width: 28rem">
-            <nuxt-img
-              :src="image"
-              class="card-img-top img-fluid"
-              style="width: 100%"
-            />
+            <nuxt-img :src="image" class="card-img-top img-fluid" style="width: 100%" />
             <div class="card-body">
               <h5 class="card-title">
                 Görsel Adı: {{ product.imageNames[index] }}
@@ -145,15 +143,8 @@ async function save() {
     </div>
     <div class="mb-3">
       <label for="product-image" class="form-label">Ürün Görselli</label>
-      <input
-        @change="onFileChange"
-        ref="input_image"
-        class="form-control"
-        type="file"
-        id="product-image"
-        accept="image/png, image/jpeg"
-        multiple
-      />
+      <input @change="onFileChange" ref="input_image" class="form-control" type="file" id="product-image"
+        accept="image/png, image/jpeg" multiple />
     </div>
     <button class="btn btn-primary" @click="save">Kaydet</button>
   </div>
--- server/api/image/index.post.ts
+++ server/api/image/index.post.ts
@@ -1,5 +1,37 @@
 
 import { postImage } from "../../data/images";
+import fs from "fs"
+
+function toUTF8Array(str) {
+  var utf8 = [];
+  for (var i=0; i < str.length; i++) {
+      var charcode = str.charCodeAt(i);
+      if (charcode < 0x80) utf8.push(charcode);
+      else if (charcode < 0x800) {
+          utf8.push(0xc0 | (charcode >> 6), 
+                    0x80 | (charcode & 0x3f));
+      }
+      else if (charcode < 0xd800 || charcode >= 0xe000) {
+          utf8.push(0xe0 | (charcode >> 12), 
+                    0x80 | ((charcode>>6) & 0x3f), 
+                    0x80 | (charcode & 0x3f));
+      }
+      // surrogate pair
+      else {
+          i++;
+          // UTF-16 encodes 0x10000-0x10FFFF by
+          // subtracting 0x10000 and splitting the
+          // 20 bits of 0x0-0xFFFFF into two halves
+          charcode = 0x10000 + (((charcode & 0x3ff)<<10)
+                    | (str.charCodeAt(i) & 0x3ff));
+          utf8.push(0xf0 | (charcode >>18), 
+                    0x80 | ((charcode>>12) & 0x3f), 
+                    0x80 | ((charcode>>6) & 0x3f), 
+                    0x80 | (charcode & 0x3f));
+      }
+  }
+  return utf8;
+}
 
 export default defineEventHandler(async (event) => {
   let response = {
@@ -7,13 +39,19 @@ export default defineEventHandler(async (event) => {
     status: false
   }
   const body = await readBody(event)
+  console.log("body",body);
 
-
-  const buffer = Buffer.from(body, "utf-8")
-  console.log("buffer",buffer);
+  // console.log("file(0)", body["file0"]);
+  // const buffer = Buffer.from(body, "utf-8")
+  // console.log("buffer",buffer);
   const data = {
-    image: buffer
+    image: body
   }
+
+
+  console.log("data",data);
+  return response
+
 
   const resImage = await postImage(data)
   console.log("resImage", resImage);
