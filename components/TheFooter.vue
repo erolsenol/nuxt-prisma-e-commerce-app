9d4452da5450528ddeb61d8bf450e0f30165ec4e
erolsenol <erol.senol@outlook.com>
2023-08-17 22:45:52 +0300

    site changes

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -6,32 +6,45 @@
    <div class="footer mt-4">
       <div class="container pt-3">
          <div class="row">
-            <div class="col-12 col-sm-6 col-md-4 mb-3">
+            <div class="col-12 col-sm-6  mb-3">
                <ul class="list-group list-group-vertical">
-                  <li class="list-group-item"> <h5>An item</h5></li>
-                  <li class="list-group-item">A second item</li>
-                  <li class="list-group-item">A third item</li>
+                  <li class="list-group-item">
+                     LOGO
+                  </li>
+                  <li class="list-group-item">
+                     <Icon name="material-symbols:call-sharp" color="white" size="30" />
+                     <span class="ms-2">+90 555 555 55 55</span>
+                  </li>
+                  <li class="list-group-item">
+                     <Icon name="material-symbols:mail" color="white" size="30" />
+                     <span class="ms-2">qweqw qwe qwe qwe qwe</span>
+
+                  </li>
+                  <li class="list-group-item">
+                     <Icon name="material-symbols:mail" color="white" size="30" />
+                     <span class="ms-2">qweqw qwe qwe qwe qwe</span>
+
+                  </li>
                </ul>
             </div>
-            <div class="col-12 col-sm-6 col-md-4 mb-3">
+            <div class="col-12 col-sm-6  mb-3">
                <ul class="list-group list-group-vertical">
-                  <li class="list-group-item">An item</li>
-                  <li class="list-group-item">A second item</li>
-                  <li class="list-group-item">A third item</li>
+                  <li class="list-group-item">Ürünler</li>
+                  <li class="list-group-item">Hakkımızda</li>
+                  <li class="list-group-item">Haberler ve Yayınlar</li>
+                  <li class="list-group-item">İletişim</li>
                </ul>
             </div>
-            <div class="col-12 col-sm-6 col-md-4 mb-3">
-               <ul class="list-group list-group-vertical">
-                  <li class="list-group-item">An item</li>
-                  <li class="list-group-item">A second item</li>
-                  <li class="list-group-item">A third item</li>
-               </ul>
-            </div>
+
          </div>
+      </div>
+      <div class="divider"></div>
+      <div class="container footer-bottom d-flex align-items-center justify-content-center py-2">
+         <span>Neva Advertising Agency | Copyright © 2021. All rights reserved. In case of copying, reproduction and
+            distribution, our legal rights will be exercised.
+         </span>
       </div>
    </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -53,21 +53,22 @@ let selectedIndex = ref("0")
    <div class="header mb-3">
       <div class="">
          <ul class="nav nav-pills nav-fill shadow-lg justify-content-center flex-column flex-sm-row mb-2">
-            <li class="nav-item me-1 logo">
-               <div class="logo-container">
+            <li class="nav-item logo">
+               <a class="nav-link shadow12 fs-5 logo-container-logo py-3" :class="selectedIndex == '0' ? 'nav-link-active' : ''"
+                  aria-current="page" href="#" @click="selectedIndex = '0'">
                   <span class="logo-container-mail fs-6">
                      erosnl@gmail.com
                   </span>
-                  <a class="nav-link fs-5 logo-container-logo py-3" :class="selectedIndex == '0' ? 'nav-link-active' : ''" aria-current="page" href="#"
-                  @click="selectedIndex = '0'">LOGO</a>
-               </div>
+                  <span>LOGO</span>
+               </a>
             </li>
-            <li class="nav-item me-1" v-for="(item, key) in headerItems" :key="key">
-               <NuxtLink class="nav-link fs-5 py-3" :class="selectedIndex == key + 1 ? 'nav-link-active' : ''"
+            <li class="nav-item " v-for="(item, key) in headerItems" :key="key">
+               <NuxtLink class="nav-link shadow12 fs-5 py-3" :class="selectedIndex == key + 1 ? 'nav-link-active' : ''"
                   @click="pageChange(key + 1, item.to)">{{ $t(item.text) }}</NuxtLink>
             </li>
             <li class="nav-item dropdown d-flex align-items-center lang">
-               <a class="nav-link fs-5 dropdown-toggle lang-item" data-bs-toggle="dropdown" role="button" aria-expanded="false">
+               <a class="nav-link shadow12 fs-5 dropdown-toggle lang-item" data-bs-toggle="dropdown" role="button"
+                  aria-expanded="false">
                   {{ locale.toUpperCase() }}
                </a>
                <ul class="dropdown-menu">
