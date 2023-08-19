d20520d4da8dab171d39abb0927ef7f39d5e58bf
erolsenol <erol.senol@outlook.com>
2023-08-19 10:02:24 +0300

    about us page contiune

---
+++ server/api/pageAbout/index.delete.ts
@@ -0,0 +1,77 @@
+import { deleteImage, getImage } from "../../data/images";
+import { deleteProduct } from "../../data/products";
+import fs from "fs";
+import path from "path";
+
+const base64Replace = (str: String) =>
+  str.replace(/^data:image\/\w+;base64,/, "");
+
+interface fileRes {
+  success: Boolean;
+  error?: String;
+}
+interface errorData {
+  name: String;
+  error?: String;
+}
+
+interface response {
+  data: errorData[];
+  status: Boolean;
+}
+
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event);
+  const id: Number = Number(query.id) as number;
+
+  let response: response = {
+    data: [],
+    status: false,
+  };
+
+  if (!id) {
+    response.error = "id cannot be empty";
+    return response;
+  }
+
+  console.log(id,typeof id);
+  const image = await getImage(id);
+
+  if (!image.id) {
+    response.error = "image not found";
+    return response;
+  }
+
+  const dir = process.cwd();
+
+  const filePath = `${dir}/public/images/${image.name}`;
+
+  const res = await deleteFile(filePath);
+  console.log("delete file:", res);
+  if (res.success) {
+    const imgRes = await deleteImage(id);
+    console.log("delete response:", imgRes);
+    if (imgRes.id) {
+      response.data.push(imgRes);
+    } else {
+      const errData: errorData = {
+        name: image.name,
+        error: "failed to write to image database",
+      };
+      response.data.push(errData);
+    }
+  } else {
+    const errData: errorData = {
+      name: image.name,
+      error: "image could not be deleted",
+    };
+    response.data.push(errData);
+  }
+
+  if (response.data.length > 0) {
+    response.status = true;
+    return response;
+  }
+
+  return response;
+});
