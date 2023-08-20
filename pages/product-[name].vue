58cf3cf377de33c89d1e51594aea6c2c73944a9a
erolsenol <erol.senol@outlook.com>
2023-08-20 19:13:43 +0300

    product detail page edit

--- pages/product-[name].vue
+++ pages/product-[name].vue
@@ -12,32 +12,49 @@ onMounted(() => {
   console.log("product onMounted");
   //fix
   setTimeout(() => {
-    get()  
+    get()
   }, 500);
-  
+
 })
 
-async function get(){
+async function get() {
 
   const config = {
     params: {
-      name : route.params.name
+      name: route.params.name
     },
     paramsSerializer: (params) => $qs.stringify(params, { encode: false })
   };
 
   const { data } = await useFetch("/api/product", config);
-  console.log("data",data.value);
+  console.log("data", data.value);
   if (!data || !data.value || !data.value.data) return
 
   item.value = data.value.data
-  console.log("item.value",item.value);
+  console.log("item.value", item.value);
 }
 
 </script>
 
 <template>
-  <div class="container">
-    <p>{{ item }}</p>
+  <div class="container product-detail">
+    <div class="row">
+      <div class="col-6 product-detail-image border rounded-2 p-2">
+        <template v-if="item.images && item.images.length > 0">
+          <NuxtImg class="rounded-2" :src="'images/' + item.images[0].name" style="width: 26rem; height: 30rem;" />
+        </template>
+        <NuxtImg class="border-bottom border-secondary-subtle" v-else :src="'images/no-image.jpeg'"
+          style="height: 15rem;" />
+      </div>
+      <div class="col-6 product-detail-content">
+        <div class="product-detail-content-title">
+          {{ item.title }}
+        </div>
+        <div class="product-detail-content-name">
+          {{ item.name }}
+        </div>
+      </div>
     </div>
+    <p>{{ item }}</p>
+  </div>
 </template>
