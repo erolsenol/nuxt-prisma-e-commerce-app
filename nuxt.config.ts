6501f124375bbc8c85f3b10ff721e9ccfcaa0d18
erolsenol <erol.senol@outlook.com>
2023-09-18 11:41:59 +0300

    change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   nitro: {
     // baseURL: "http://localhost:80",
+    preset: 'node-server',
     prerender: {
       // crawlLinks: true,
       // failOnError: false
